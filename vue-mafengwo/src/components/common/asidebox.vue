01180603794e833567975f536aa0b38a776754a4
cysgg <279709624@qq.com>
2019-10-17 22:48:42 +0800

    公共组件

---
+++ vue-mafengwo/src/components/common/asidebox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="asidebox-hd">
+      <h2 class="hd-title">
+        <router-link to="" tag="a">
+          {{title}}
+        </router-link>
+      </h2>
+      <router-link class="hd-right" to="" tag="a">
+        {{headRight}}
+      </router-link>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'asidebox',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    headRight: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.asidebox-hd
+  height 35px
+  line-height 35px
+  margin-bottom 12px
+  .hd-title
+    float left
+    color #333
+    font-weight normal
+    a
+      color #333
+      text-decoration none
+      font-size 18px
+  .hd-right
+    float right
+    color #999
+</style>
---
+++ vue-mafengwo/src/components/common/slider.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="slide-ul">
+      <slot
+        v-if="showInfoList && showInfoList.length > 0"
+        name="showArea"
+        :currItem="currItem"
+      ></slot>
+    </div>
+    <div class="slide-ol">
+      <div
+        v-for="(item, index) in showInfoList"
+        :key="index + item"
+        @click="selectItem(index)"
+      >
+        <slot
+          v-if="showInfoList && showInfoList.length > 0"
+          name="clickArea"
+          :scopeItem="{currIndex, index, item}"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'slider',
+  props: {
+    showInfoList: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      currIndex: 0,
+      ivTimer: null
+    }
+  },
+  computed: {
+    currItem () {
+      return this.showInfoList[this.currIndex]
+    }
+  },
+  mounted () {
+    this.ivTimer = setInterval(this.loopItem, 10000)
+  },
+  methods: {
+    selectItem (val) {
+      if (val > this.currIndex) {
+        this.$emit('slideToRight')
+      } else {
+        this.$emit('slideToLeft')
+      }
+      this.currIndex = val
+    },
+    loopItem () {
+      this.selectItem((this.currIndex + 1) % this.showInfoList.length)
+    }
+  },
+  beforeDestroy () {
+    clearInterval(this.ivTimer)
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.slide-ul
+  position absolute
+  left 0
+  top 0
+  width 100%
+  height 100%
+.slide-ol
+  display inline-block
+  position absolute
+  right 14px
+  top 120px
+  width 90px
+</style>
