0833c4c2744b504cb5d7edac4bf6774af1e52e9b
cysg <124589>
2020-07-15 14:19:36 +0800

    test

--- vue-true/src/components/tree.vue
+++ vue-true/src/components/tree.vue
@@ -1,49 +1,50 @@
 <!-- src/components/tree/tree.vue -->
 <template>
-  <div>
-    <tree-node
-      v-for="(item, index) in cloneData"
-      :key="index"
-      :data="item"
-      :show-checkbox="showCheckbox"
-    ></tree-node>
-  </div>
-</template> <script>
-import TreeNode from "./node.vue";
-import { deepCopy } from "../utils";
+    <div>
+        <tree-node
+            v-for="(item, index) in cloneData"
+            :key="index"
+            :data="item"
+            :show-checkbox="showCheckbox"
+        ></tree-node>
+    </div>
+</template>
+<script>
+import TreeNode from './node.vue'
+import { deepCopy } from '../utils'
 
 export default {
-  name: "Tree",
-  components: { TreeNode },
-  props: {
-    data: {
-      type: Array,
-      default() {
-        return [];
-      }
+    name: 'Tree',
+    components: { TreeNode },
+    props: {
+        data: {
+            type: Array,
+            default() {
+                return []
+            }
+        },
+        showCheckbox: {
+            type: Boolean,
+            default: false
+        }
     },
-    showCheckbox: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data() {
-    return {
-      cloneData: []
-    };
-  },
-  created() {
-    this.rebuildData();
-  },
-  watch: {
     data() {
-      this.rebuildData();
+        return {
+            cloneData: []
+        }
+    },
+    created() {
+        this.rebuildData()
+    },
+    watch: {
+        data() {
+            this.rebuildData()
+        }
+    },
+    methods: {
+        rebuildData() {
+            this.cloneData = deepCopy(this.data)
+        }
     }
-  },
-  methods: {
-    rebuildData() {
-      this.cloneData = deepCopy(this.data);
-    }
-  }
-};
+}
 </script>
