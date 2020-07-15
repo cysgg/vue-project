af25a961b718ea8ad47ba7d27f95c6a38b939ff8
cysg <124589>
2020-07-15 11:14:04 +0800

    test

---
+++ vue-true/src/components/tree.vue
@@ -0,0 +1,49 @@
+<!-- src/components/tree/tree.vue -->
+<template>
+  <div>
+    <tree-node
+      v-for="(item, index) in cloneData"
+      :key="index"
+      :data="item"
+      :show-checkbox="showCheckbox"
+    ></tree-node>
+  </div>
+</template> <script>
+import TreeNode from "./node.vue";
+import { deepCopy } from "../utils";
+
+export default {
+  name: "Tree",
+  components: { TreeNode },
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    showCheckbox: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      cloneData: []
+    };
+  },
+  created() {
+    this.rebuildData();
+  },
+  watch: {
+    data() {
+      this.rebuildData();
+    }
+  },
+  methods: {
+    rebuildData() {
+      this.cloneData = deepCopy(this.data);
+    }
+  }
+};
+</script>
